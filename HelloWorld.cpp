bec192d0365ef73919834d98e1731295206d6d2c
Abdur Rakhman <abdurrahmansy25@gmai.com>
2022-06-13 18:36:10 +0300

    loops

---
+++ HelloWorld.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+  //syntax while
+  int x = 0;
+          while (x <= 3) {
+          cout << x << endl;
+           x++;
+  }
+     // syntax do while
+     int y = 0;
+     do {
+          cout << y << endl;
+          y++;     
+          }
+          while (y < 5);
+     
+     //   next syntax for loops
+          for (int z = 0; z < 10 ; z++){
+               cout << z << endl;
+          }
+          
+
+
+  return 0;
+}
